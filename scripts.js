081aee80d704f980b08fda9412f181e192aa17b6
Enashpinal <409894128@qq.com>
2024-07-23 13:23:53 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,3 +8,9 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         });
     });
 });
+function setRandomBackground() {
+    const imgUrl = 'https://api.imlazy.ink/img';
+    document.body.style.backgroundImage = `url(${imgUrl})`;
+}
+
+window.onload = setRandomBackground;
