8c17bd4a0fc829486ce4889f46624f4d4ea48c41
Enashpinal <409894128@qq.com>
2024-07-26 15:18:55 +0800

    Update smoothScroll.js

--- smoothScroll.js
+++ smoothScroll.js
@@ -1,7 +1,7 @@
-let scrollSpeed = 10;
+let scrollSpeed = 20;
 let currentScroll = window.scrollY; 
 let targetScroll = currentScroll;
-let inertia = 0.3; 
+let inertia = 0.001; 
 
 function smoothScroll() {
     currentScroll += (targetScroll - currentScroll) * inertia;
