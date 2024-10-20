bdde9d4e23f0ded7b54d75275a5214dd27a8434b
Enashpinal <409894128@qq.com>
2024-10-21 00:28:30 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -6,11 +6,7 @@ export async function onRequest(context) {
         const response = await fetch(url, {
             method: 'GET',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.5',
-                'Connection': 'keep-alive',
-                // 其他必要的请求头可以在这里添加
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
             }
         });
 
