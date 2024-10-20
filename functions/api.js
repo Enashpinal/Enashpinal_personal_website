115e1e79e0820dd5acbbe36a76ea0f88f150903f
Enashpinal <409894128@qq.com>
2024-10-21 00:09:24 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -6,13 +6,7 @@ export async function onRequest(context) {
     try {
         // 使用fetch请求获取HTML内容
         const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                // 设置适当的请求头
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.5',
-            },
+            method: 'GET'
         });
 
         // 检查请求状态
