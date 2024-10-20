50457b992fdc2dcd81d3470ee9ce8abff3e93f0e
Enashpinal <409894128@qq.com>
2024-10-20 23:51:17 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,24 +1,15 @@
-async function fetchHTML(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error('网络请求失败');
-    }
-
-    const text = await response.text();
-    return text;
-}
-
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
+export async function onRequest(context) {
     const seedURL = 'https://www.bilibili.com/v/popular/rank/all';
-    
-    try {
-        const htmlContent = await fetchHTML(seedURL);
 
-        // 这里可以将 HTML 内容直接放入 JSON 对象中
+    try {
+        const response = await fetch(seedURL);
+        if (!response.ok) {
+            return new Response('网络请求失败', { status: 500 });
+        }
+
+        const htmlContent = await response.text();
+
+        // 将 HTML 内容放入 JSON 对象中
         const jsonResponse = {
             source: "哔哩哔哩排行榜",
             html: htmlContent
